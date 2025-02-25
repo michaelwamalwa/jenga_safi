e4e647949c8c52c68224df3e7b7153d23ee75c38
Wamalwa <andikamichael163@gmail.com>
2025-02-25 16:26:28 +0300

    Fixed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 typeScript: {
   ignoreBuildErrors: true,
-}
+},
+productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
