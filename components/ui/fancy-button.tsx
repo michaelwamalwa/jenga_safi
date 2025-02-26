7e3bfeb45240e7b610c33f280228cdf9956f50f2
Wamalwa <andikamichael163@gmail.com>
2025-02-26 20:11:11 +0300

    Fixed

--- components/ui/fancy-button.tsx
+++ components/ui/fancy-button.tsx
@@ -3,19 +3,21 @@ import { FC, ReactNode } from "react";
 interface ButtonProps {
   text: string;
   icon: ReactNode;
+  onClick?: () => void; // Make it clickable
 }
 
-const FancyButton: FC<ButtonProps> = ({ text, icon }) => {
+const FancyButton: FC<ButtonProps> = ({ text, icon, onClick }) => {
   return (
-    <a className="fancy-btn">
-      <div className="group bg-black hover:bg-transparent text-primary-foreground hover:text-black rounded-[108em] py-5 px-10 flex items-center gap-2 font-bold text-3xl ">
-        <span>{text}</span>
-        <span className="group-hover:translate-x-[.75vw] transition-transform duration-100">
-          {icon}
-        </span>
-      </div>
-      {/* Inner content */}
-    </a>
+    <button 
+      className="fancy-btn group bg-black hover:bg-transparent text-primary-foreground hover:text-black 
+                 rounded-[108em] py-5 px-10 flex items-center gap-2 font-bold text-3xl touch-manipulation"
+      onClick={onClick} // Ensure click works
+    >
+      <span>{text}</span>
+      <span className="group-hover:translate-x-[.75vw] transition-transform duration-100">
+        {icon}
+      </span>
+    </button>
   );
 };
 
